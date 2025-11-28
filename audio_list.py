46ab289cfab5ecd7ff9dffc47c531b25e5773ad8
Josep Crego <jmcrego@gmail.com>
2025-11-28 17:37:00 +0100

    light

--- audio_list.py
+++ audio_list.py
@@ -54,7 +54,7 @@ def find_audio_files_by_lang(base_path, langs, max_files_lang, output):
                 filepath, duration = get_audio_duration(filepath)
                 if duration is not None:
                     total_duration += duration
-                fdo.write(f"{lang}\t{duration:.12f}\t{filepath}\n")
+                fdo.write(f"{lang}\t{duration:.2f}\t{filepath}\n")
 
             sys.stderr.write(f"Lang {lang}, Total files {len(files)}, Total duration for {lang}: {total_duration:.2f}s ({total_duration/3600:.2f}h)\n")
     
