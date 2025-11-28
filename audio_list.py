cb56f442207b2a90737200b1f26e47ebbb9ff337
Josep Crego <jmcrego@gmail.com>
2025-11-28 18:18:02 +0100

    light

--- audio_list.py
+++ audio_list.py
@@ -52,17 +52,23 @@ def find_audio_files_by_lang(base_path, langs, max_files_lang, min_duration_file
 
             bar = tqdm(total=max_files_lang or len(files), desc=f"{lang} files", unit=" file")
             n_files = 0
+            n_discarded = 0
             for filepath in files:
                 filepath, duration = get_audio_duration(filepath)
                 if duration is not None:
                    if min_duration_file is None or duration > min_duration_file:
                         total_duration += duration
+                    else:
+                       n_discarded += 1
+                       continue
+                else:
+                    n_discarded += 1
+                    continue
                 fdo.write(f"{lang}\t{duration:.2f}\t{filepath}\n")
                 bar.update(1)
                 n_files += 1
                 if max_files_lang is not None and n_files >= max_files_lang:
                     break
-
 
             sys.stderr.write(f"Lang {lang}, Total files {n_files}, Total duration {total_duration:.2f}s ({total_duration/3600:.2f}h)\n")
             fdo.write(f"{lang} TotalDuration={total_duration/3600:.2f}h\n")
