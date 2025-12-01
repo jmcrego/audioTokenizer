8dead99c63d19d7f7e17d79e435b74d874b0dcf6
Josep Crego <jmcrego@gmail.com>
2025-12-01 12:22:45 +0100

    light

--- Utils.py
+++ Utils.py
@@ -56,18 +56,18 @@ def list_audio_files(path: str):
     return sorted(files)
 
 def arguments(args):
+    # args.pop('audio_processor', None)
+    # args.pop('audio_embedder', None)
+    # args.pop('audio_tokenizer', None)
     args.pop('self', None)  # None prevents KeyError if 'self' doesn't exist
-    if 'audio_processor' in args and 'meta' in args['audio_processor']:
+    if 'audio_processor' in args: #and 'meta' in args['audio_processor']:
         args['audio_processor'] = args['audio_processor']['meta']
-        # args.pop('audio_processor', None)
 
-    if 'audio_embedder' in args and 'meta' in args['audio_embedder']:
+    if 'audio_embedder' in args: #and 'meta' in args['audio_embedder']:
         args['audio_embedder'] = args['audio_embedder']['meta']
-        # args.pop('audio_embedder', None)
 
-    if 'audio_tokenizer' in args and 'meta' in args['audio_tokenizer']:
+    if 'audio_tokenizer' in args: #and 'meta' in args['audio_tokenizer']:
         args['audio_tokenizer'] = args['audio_tokenizer']['meta']
-        # args.pop('audio_tokenizer', None)
     return args
 
 
