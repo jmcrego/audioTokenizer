caef21eab33e91c547d4f8b2a6cfd88bef364073
Josep Crego <jmcrego@gmail.com>
2025-12-09 10:39:52 +0100

    light

--- Datasets.py
+++ Datasets.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     from transformers import AutoTokenizer
 
     tokenizer = AutoTokenizer.from_pretrained("/lustre/fsmisc/dataset/HuggingFace_Models/utter-project/EuroLLM-1.7B-Instruct", use_fast=True)
-    dataset = AudioIterableDataset(sys.argv[1], tokenizer)
+    dataset = AudioDataset(sys.argv[1], tokenizer)
     for i,e in enumerate(dataset): 
         pass
 
