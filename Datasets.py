4ae64f14103394c455ca5dad4c2bffd9fc422b5e
Josep Crego <jmcrego@gmail.com>
2025-12-09 10:41:17 +0100

    light

--- Datasets.py
+++ Datasets.py
@@ -107,5 +107,5 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("/lustre/fsmisc/dataset/HuggingFace_Models/utter-project/EuroLLM-1.7B-Instruct", use_fast=True)
     dataset = AudioDataset(sys.argv[1], tokenizer)
     for i,e in enumerate(dataset): 
-        pass
+        print(e)
 
