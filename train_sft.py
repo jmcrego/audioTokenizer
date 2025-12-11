25e7bc684ea9c7f610e1e7a98f96d0a11ff8fe98
Josep Crego <jmcrego@gmail.com>
2025-12-11 10:44:18 +0100

    light

--- train_sft.py
+++ train_sft.py
@@ -1,19 +1,20 @@
+
 import os
 import torch
 import logging
 import argparse
 import subprocess
 import numpy as np
+
 from trl import SFTTrainer, SFTConfig
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import Dataset as HFDataset
 
 from AudioEmbedder import AudioEmbedder
 from AudioToLLMProjector import AudioToLLMProjector
 from Datasets import AudioDataset, BatchedLengthSampler
-
-from datasets import Dataset as HFDataset
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
