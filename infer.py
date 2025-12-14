a0687ccd0cd2cb89012e860e664ca20b7dcd3f7b
Josep Crego <jmcrego@gmail.com>
2025-12-14 12:43:59 +0100

    bug fix

--- infer.py
+++ infer.py
@@ -41,8 +41,8 @@ if __name__ == "__main__":
         ]
     )
 
-    logger.info("CUDA available:", torch.cuda.is_available())
-    logger.info("Device count:", torch.cuda.device_count())
+    logger.info(f"CUDA available: {torch.cuda.is_available()}")
+    logger.info(f"Device count: {torch.cuda.device_count()})
 
     device, dtype = get_device_dtype()
     logger.info(f"device: {device}, dtype: {dtype}")
