dc1872d671cc330937869154a87085069a3cb838
Josep Crego <jmcrego@gmail.com>
2025-12-18 08:16:51 +0100

    bug fix

--- infer.py
+++ infer.py
@@ -55,13 +55,6 @@ if __name__ == "__main__":
     with open(args.config, "r", encoding="utf-8") as file:
         config = json.load(file)
 
-    # audio = config["audio"]
-    # projector = config["projector"]
-    # llm_path = config["llm"]["path"]
-    # lora_path = config["lora"]["path"]
-    # asr_token = config["asr_token"]
-    # stt_token = config["stt_token"]
-
     # --------------------------------------------------
     # Task → prompt
     # --------------------------------------------------
