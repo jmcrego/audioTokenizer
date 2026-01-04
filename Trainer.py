7e286c79b7cb3db2bfbd82508aa3437492dd3a0b
Josep Crego <jmcrego@gmail.com>
2026-01-04 13:27:47 +0100

    bug fix

--- Trainer.py
+++ Trainer.py
@@ -161,7 +161,6 @@ class Trainer:
         # Save config file after updating lora path
         self.config['lora']['path'] = ckpt_path + ".lora"
         self.config['projector']['path'] = ckpt_path + ".proj.pt"
-        self.config['llm']['add_tokens']['path'] = ckpt_path + ".embs.pt"
         with open(f"{ckpt_path}.config.json", "w", encoding="utf-8") as file:
             json.dump(self.config, file, indent=4)
         logger.info(f"Saved config to {ckpt_path}.config.json")
