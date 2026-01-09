0e2297509417b2fb6f3a25fdb10a7ec8ebf1ca6e
Josep Crego <jmcrego@gmail.com>
2026-01-09 13:17:11 +0100

    debug

--- scripts/link_covost_tsv.py
+++ scripts/link_covost_tsv.py
@@ -1,5 +1,6 @@
 # coding=utf-8                                                                                                                                                                                                                                                                                    
 import os
+import io
 import sys
 import argparse
 import tarfile
